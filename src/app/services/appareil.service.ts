f9fddabbfbe1a94bdebc1717cd47d43b750313da
Youplaf <37189944+Youplaf@users.noreply.github.com>
2018-04-08 19:14:22 +0200

    first commit

---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,44 @@
+export class AppareilService {
+    appareils: any [] = [ 
+        {
+            id: 1,
+            name: 'Machine à laver',
+            status: 'éteint'
+        },
+        {
+            id: 2,
+            name: 'Télévision',
+            status: 'allumé'
+        },
+        {
+            id: 3,
+            name: 'Ordinateur',
+            status: 'éteint'
+        },
+    ];
+
+    getAppareilById(id: number) {
+        const appareil = this.appareils.find(appareil => appareil.id === id);
+        return appareil;
+    }
+
+    switchOnAll() {
+        for(let appareil of this.appareils) {
+            appareil.status = 'allumé';
+        }
+    }
+
+    switchOffAll() {
+        for(let appareil of this.appareils) {
+            appareil.status = 'éteint';
+        }
+    }
+
+    switchOnOne(index: number) {
+        this.appareils[index].status= 'allumé';
+    }
+
+    switchOffOne(index: number) {
+        this.appareils[index].status= 'éteint';
+    }
+}
